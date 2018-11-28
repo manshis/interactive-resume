e9708851c5e469c8e6ce7a7bdd3755bce47af63c
Manshi Sharma <ece.manshi@gmail.com>
2018-11-28 18:01:49 +0530

    added top content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatIconModule, MatGridListModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopSectionComponent } from './top-section/top-section.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopSectionComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/top-section/top-section.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-section',
+  templateUrl: './top-section.component.html',
+  styleUrls: ['./top-section.component.css']
+})
+export class TopSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  scrollDown() {
+    window.scroll({
+      top: 720, 
+      left: 0, 
+      behavior: 'smooth'
+    });
+  }
+}
